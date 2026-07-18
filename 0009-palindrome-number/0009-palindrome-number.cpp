7962329a09b6c7cb55d78a2c397bc6fcae485219
Nimit Jain <jainnimit719@gmail.com>
2026-07-18 23:12:10 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long reverse=0;
+        long temp = x;
+
+        while(temp>0){
+            long digit = temp % 10;
+            temp = temp/10;
+            reverse = reverse*10 + digit;
+        }
+        
+        if(x == reverse ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
